165d0b6b7d26db4260c4a87dc6df86f58ff933e6
Juho Leinonen <juholei@gmail.com>
2017-01-24 19:23:38 +0200

    Add function to remove column from parsed csv. Comment out db tests as there is no db at the moment.

--- src/clj/my_money/op_csv.clj
+++ src/clj/my_money/op_csv.clj
@@ -2,4 +2,12 @@
   (:require [clojure.data.csv :as csv]))
 
 (defn read-csv [csv-string]
-    (csv/read-csv csv-string :separator \;))
+  (csv/read-csv csv-string :separator \;))
+
+(defn remove-column [csv-vec column]
+  "Removes item in given column number from each vector in the vector of vectors"
+  (let [reducer (fn [acc row]
+                  (conj acc
+                        (vec (concat (subvec row 0 column)
+                                     (subvec row (inc column))))))]
+    (reduce reducer [] csv-vec)))
--- test/clj/my_money/test/op_csv.clj
+++ test/clj/my_money/test/op_csv.clj
@@ -2,10 +2,18 @@
   (:require [clojure.test :refer :all]
             [my-money.op-csv :refer :all]))
 
-(def test-csv "Kirjauspäivä;Arvopäivä;Määrä  EUROA;\"Laji\";Selitys;Saaja/Maksaja;Saajan tilinumero ja pankin BIC;Viite;Viesti;Arkistointitunnus;")
+(def test-csv "Kirjauspäivä;Arvopäivä;Määrä  EUROA;\"Laji\";Selitys;Saaja/Maksaja;Saajan tilinumero ja pankin BIC;Viite;Viesti;Arkistointitunnus;\n24.01.2017;24.01.2017;500,00;\"123\";TALLETUSAUTOM.;\"PERSON EXAMPLE\";;\"\";Käteistalletus automaatilla 12345 Setelit yht 500,00 EUR Setelit yht 1 kpl  ;2412231/876567/9412363")
 
 (deftest test-parsing-column-names-from-csv
   (testing "Column names"
     (is (= ["Kirjauspäivä" "Arvopäivä" "Määrä  EUROA" "Laji"
             "Selitys" "Saaja/Maksaja" "Saajan tilinumero ja pankin BIC"
             "Viite" "Viesti" "Arkistointitunnus" ""] (first (read-csv test-csv))))))
+
+(deftest test-removing-columns-from-parsed-csv
+  (testing "Removing a column"
+    (is (= [["Kirjauspäivä" "Määrä  EUROA" "Laji"
+            "Selitys" "Saaja/Maksaja" "Saajan tilinumero ja pankin BIC"
+            "Viite" "Viesti" "Arkistointitunnus" ""]
+            ["24.01.2017" "500,00" "123" "TALLETUSAUTOM." "PERSON EXAMPLE" "" "" "Käteistalletus automaatilla 12345 Setelit yht 500,00 EUR Setelit yht 1 kpl  " "2412231/876567/9412363"]]
+           (remove-column (read-csv test-csv) 1)))))
