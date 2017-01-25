f6edf619b679d2e73bb6767edb8c9834b2b3834f
Juho Leinonen <juholei@gmail.com>
2017-01-25 20:01:35 +0200

    Fix formatting

--- test/clj/my_money/test/op_csv.clj
+++ test/clj/my_money/test/op_csv.clj
@@ -13,23 +13,23 @@
 (deftest test-removing-columns-from-parsed-csv
   (testing "Removing a column"
     (is (= [["Kirjauspäivä" "Määrä  EUROA" "Laji"
-            "Selitys" "Saaja/Maksaja" "Saajan tilinumero ja pankin BIC"
-            "Viite" "Viesti" "Arkistointitunnus" ""]
+             "Selitys" "Saaja/Maksaja" "Saajan tilinumero ja pankin BIC"
+             "Viite" "Viesti" "Arkistointitunnus" ""]
             ["24.01.2017" "500,00" "123" "TALLETUSAUTOM." "PERSON EXAMPLE" "" "" "Käteistalletus automaatilla 12345 Setelit yht 500,00 EUR Setelit yht 1 kpl  " "2412231/876567/9412363"]]
            (remove-column (read-csv test-csv) 1)))))
 
 (deftest test-removing-column-by-name
   (testing "Removing column with given name"
     (is (= [["Kirjauspäivä" "Määrä  EUROA" "Laji"
-            "Selitys" "Saaja/Maksaja" "Saajan tilinumero ja pankin BIC"
-            "Viite" "Viesti" "Arkistointitunnus" ""]
+             "Selitys" "Saaja/Maksaja" "Saajan tilinumero ja pankin BIC"
+             "Viite" "Viesti" "Arkistointitunnus" ""]
             ["24.01.2017" "500,00" "123" "TALLETUSAUTOM." "PERSON EXAMPLE" "" "" "Käteistalletus automaatilla 12345 Setelit yht 500,00 EUR Setelit yht 1 kpl  " "2412231/876567/9412363"]]
-            (remove-column-by-name (read-csv test-csv) "Arvopäivä")))))
+           (remove-column-by-name (read-csv test-csv) "Arvopäivä")))))
 
 (deftest test-giving-incorrect-column-name-removes-nothing
   (testing "Giving incorrect column name removes nothing"
     (is (= [["Kirjauspäivä" "Arvopäivä" "Määrä  EUROA" "Laji"
-            "Selitys" "Saaja/Maksaja" "Saajan tilinumero ja pankin BIC"
-            "Viite" "Viesti" "Arkistointitunnus" ""]
+             "Selitys" "Saaja/Maksaja" "Saajan tilinumero ja pankin BIC"
+             "Viite" "Viesti" "Arkistointitunnus" ""]
             ["24.01.2017" "24.01.2017" "500,00" "123" "TALLETUSAUTOM." "PERSON EXAMPLE" "" "" "Käteistalletus automaatilla 12345 Setelit yht 500,00 EUR Setelit yht 1 kpl  " "2412231/876567/9412363"]]
-            (remove-column-by-name (read-csv test-csv) "VERY IMPORTANT COLUMN")))))
+           (remove-column-by-name (read-csv test-csv) "VERY IMPORTANT COLUMN")))))
