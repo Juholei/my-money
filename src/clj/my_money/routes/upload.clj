dd263deddb32e04d0cde23aee71147ab092df0a5
Juho Leinonen <juholei@gmail.com>
2017-03-16 19:19:16 +0200

    Make the rytöläjä that is upload.clj a bit cleaner and also send the number of saved rows in the response

--- src/clj/my_money/routes/upload.clj
+++ src/clj/my_money/routes/upload.clj
@@ -4,25 +4,31 @@
             [compojure.core :refer [defroutes POST]]
             [ring.util.http-response :as response]))
 
+
+(defn- save-events [user-id events]
+  (loop [events events
+         total-rows 0]
+    (if-let [event (first events)]
+      (let [inserted-rows (db/create-event!
+                            {:id (:Arkistointitunnus event)
+                             :user-id user-id
+                             :transaction-date (:Kirjauspäivä event)
+                             :amount (Integer/parseInt (clojure.string/replace (:MääräEUROA event) "," ""))
+                             :recipient (:Saaja/Maksaja event)
+                             :type (:Laji event)})]
+        (recur (rest events) (+ total-rows inserted-rows)))
+      total-rows)))
+
 (defroutes upload-routes
   (POST "/upload" [file username]
     (when-not (db/get-user-by-username {:username username})
       (db/create-user! {:username username}))
 
-    (let [data (-> (:tempfile file)
+    (let [user-id (:id (db/get-user-by-username {:username username}))
+          data (-> (:tempfile file)
                    (slurp :encoding "ISO-8859-1")
                    (read-csv)
                    (remove-columns-by-name ["Arvopäivä" "Laji"])
                    (csv-vec->map))]
-      (let [user-id (:id (db/get-user-by-username {:username username}))]
-        (loop [events data]
-          (when-let [event (first events)]
-            (db/create-event! {:id (:Arkistointitunnus event)
-                               :user-id user-id
-                               :transaction-date (:Kirjauspäivä event)
-                               :amount (Integer/parseInt (clojure.string/replace (:MääräEUROA event) "," ""))
-                               :recipient (:Saaja/Maksaja event)
-                               :type (:Laji event)})
-            (recur (rest events)))))
-      (-> (response/ok data)))))
+      (-> (response/ok (save-events user-id data))))))
 
