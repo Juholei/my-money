8dcb30ba26f7c0fcb06d802cf5655c92ee61633c
Juho Leinonen <juholei@gmail.com>
2017-03-16 19:54:54 +0200

    Remove stupid timeout from submit page. After submission switch to events page

--- src/cljs/my_money/submit.cljs
+++ src/cljs/my_money/submit.cljs
@@ -1,13 +1,14 @@
 (ns my-money.submit
     (:require [reagent.core :as r]
-              [ajax.core :refer [GET POST]]))
+              [ajax.core :refer [GET POST]]
+              [secretary.core :as secretary]))
 
 (def username (r/atom nil))
 
-(def response-data (r/atom nil))
-
 (defn upload-response-handler [response]
-    (reset! response-data response))
+  (js/console.log (str "response handler" response))
+  (secretary/dispatch! "/events"))
+
 
 (defn upload []
   (let [file-input (.getElementById js/document "file-input")
@@ -17,8 +18,7 @@
                     (.append "file" file file-name)
                     (.append "username" @username))]
     (POST "/upload" {:body form-data
-                     :handler upload-response-handler
-                     :timeout 100})))
+                     :handler upload-response-handler})))
 
 (defn submit-page []
   [:div.container
