86d5cf9f6c71a56e3489ed27724821282dcc30e9
Juho Leinonen <juholei@gmail.com>
2017-04-14 18:38:59 +0300

    Start cleaning event filtering by type

--- test/cljs/my_money/doo_runner.cljs
+++ test/cljs/my_money/doo_runner.cljs
@@ -2,9 +2,11 @@
   (:require [doo.runner :refer-macros [doo-tests]]
             [my-money.core-test]
             [my-money.calculations-test]
-            [my-money.event-filters-test]))
+            [my-money.event-filters-test]
+            [my-money.events-test]))
 
 (doo-tests 'my-money.core-test
            'my-money.calculations-test
-           'my-money.event-filters-test)
+           'my-money.event-filters-test
+           'my-money.events-test)
 
