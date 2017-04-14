9be85cbca15fe06aad0c607c99e381653b8503f4
Juho Leinonen <juholei@gmail.com>
2017-04-14 19:12:00 +0300

    Refactor events.cljs

--- test/cljs/my_money/event_filters_test.cljs
+++ test/cljs/my_money/event_filters_test.cljs
@@ -10,3 +10,25 @@
                           {:transaction_date "11.01.2017"}
                           {:transaction_date "10.02.2017"}
                           {:transaction_date "01.02.2017"}]))))
+
+(def events [{:amount 100}
+             {:amount -100}
+             {:amount 200}])
+
+(deftest test-only-income-events
+  (let [income-filter (filters/event-filter "incomes")]
+    (is (= [{:amount 100}
+            {:amount 200}]
+           (filter income-filter events)))))
+
+(deftest test-only-expense-events
+  (let [expense-filter (filters/event-filter "expenses")]
+    (is (= [{:amount -100}]
+           (filter expense-filter events)))))
+
+(deftest test-all-events
+  (let [all-filter (filters/event-filter "all")]
+    (is (= [{:amount 100}
+            {:amount -100}
+            {:amount 200}]
+           (filter all-filter events)))))
