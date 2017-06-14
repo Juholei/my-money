a73e1096c758cb33326980094aa02db5ba84cf68
Juho Leinonen <juholei@gmail.com>
2017-06-14 19:03:01 +0300

    Use modal for uploading
    - As submit page was removed, also alerts were removed for now.

---
+++ src/cljs/my_money/components/upload.cljs
@@ -0,0 +1,42 @@
+(ns my-money.components.upload
+  (:require [ajax.core :refer [GET POST]]
+            [reagent.core :as r]
+            [reagent.session :as session]
+            [my-money.components.common :as c]
+            [my-money.events :as events]))
+
+(defn upload-response-handler [response username]
+  (events/get-events @username)
+  (session/remove! :modal))
+
+(defn upload [username]
+  (let [file-input (.getElementById js/document "file-input")
+        file (aget (.-files file-input) 0)
+        file-name (.-name file)
+        form-data (doto (js/FormData.)
+                    (.append "file" file file-name)
+                    (.append "username" @username))]
+    (POST "/upload" {:body form-data
+                     :handler #(upload-response-handler % username)})))
+
+(defn- upload-form [username]
+  [:div.form
+   [:div.form-group
+    [:label "Add your bank csv"
+     [:input.form-control-file {:type "file"}]]]
+   [:div.form-group
+    [:label "Username"
+     [:input {:class "form-control"
+              :type "text"
+              :value @username
+              :on-change #(reset! username (-> % .-target .-value))}]]]])
+
+(defn upload-button [username]
+  [:button {:class "btn btn-primary"
+            :on-click #(upload username)}
+           "Submit"])
+
+(defn upload-modal []
+  (let [username (r/atom nil)]
+    (fn []
+      [c/modal "Upload" [upload-form username] [upload-button username]])))
