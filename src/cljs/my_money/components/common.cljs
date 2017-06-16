0612d06271bdacc70dc7dfd797809c21cf495e0f
Juho Leinonen <juholei@gmail.com>
2017-06-16 16:50:17 +0300

    Close modal when its background is clicked

--- src/cljs/my_money/components/common.cljs
+++ src/cljs/my_money/components/common.cljs
@@ -2,14 +2,17 @@
   (:require [reagent.core :as r]
             [reagent.session :as session]))
 
+(defn- close-modal []
+  (session/remove! :modal))
+
 (defn modal [header body footer]
   [:div
    [:div.modal-dialog
     [:div.modal-content
      [:div.modal-header [:span.modal-title.h5 header]
                         [:button.close {:type "button"
-                                        :on-click #(session/remove! :modal)}
+                                        :on-click close-modal}
                                        "×"]]
      [:div.modal-body body]
      [:div.modal-footer footer]]]
-   [:div.modal-backdrop.fade.in]])
+   [:div.modal-backdrop.fade.in {:on-click close-modal}]])
