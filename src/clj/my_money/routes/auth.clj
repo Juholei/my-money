41ab452eea92b32715dec4d016de5e80d2f695ca
Juho Leinonen <juholei@gmail.com>
2017-07-19 15:00:43 +0300

    Implement login

--- src/clj/my_money/routes/auth.clj
+++ src/clj/my_money/routes/auth.clj
@@ -12,8 +12,24 @@
       (response/ok)
       (assoc :session (assoc session :identity (:username params)))))
 
-(defn login![req]
-  (println req))
+(defn decode-auth [encoded]
+  (let [auth (second (.split encoded " "))]
+    (-> (.decode (java.util.Base64/getDecoder) auth)
+        (String. (java.nio.charset.Charset/forName "UTF-8"))
+        (.split ":"))))
+
+(defn authenticate [[username password]]
+  (if-let [user (db/get-user-by-username {:username username})]
+    (when (hashers/check password (:password user))
+      username)))
+
+(defn login! [{:keys [session headers]}]
+  (if-let [user (authenticate (decode-auth (get headers "authorization")))]
+    (-> (:result :ok)
+        (response/ok)
+        (assoc :session (assoc session :identity user)))
+    (response/unauthorized {:result :unauthorized
+                            :message "login failure"})))
 
 (defroutes auth-routes
   (POST "/register" request
--- src/cljs/my_money/components/login.cljs
+++ src/cljs/my_money/components/login.cljs
@@ -1,11 +1,25 @@
 (ns my-money.components.login
   (:require [my-money.components.common :as c]
             [ajax.core :as ajax]
+            [goog.crypt.base64 :as b64]
             [reagent.core :as r]
             [reagent.session :as session]))
 
-(defn login! [data])
+(defn encode-auth [username password]
+  (->> (str username ":" password)
+       b64/encodeString
+       (str "Basic ")))
 
+(defn- login-handler [data]
+  (session/remove! :modal)
+  (session/put! :identity (:username data))
+  (reset! data {}))
+
+(defn login! [data]
+  (let [username (:username @data)
+        password (:password @data)]
+    (ajax/POST "/login" {:headers {"Authorization" (encode-auth username password)}
+                         :handler #(login-handler data)})))
 
 (defn- buttons [data]
   [:div
