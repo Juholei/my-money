6c5ccf97678140d41bd629605d9e390acebd5fc4
Juho Leinonen <juholei@gmail.com>
2017-09-12 18:40:00 +0300

    Add € to chart tooltip after sums

--- src/cljs/my_money/charts.cljs
+++ src/cljs/my_money/charts.cljs
@@ -19,18 +19,23 @@
                               :sum (calc/sum-til-date date events)}))]
     (sort-by :date < (reduce date-sum [] dates))))
 
+(defn- moneyfy-y-label [tooltip]
+  (str (.-yLabel tooltip) "€"))
+
 (defn chart [data]
   (let [collapsed? (r/atom false)]
     (fn [data]
-      (let [date-sums (events->date-sums data)]
+      (let [date-sums (events->date-sums data)
+            chart-options {:width 400
+                           :height 100
+                           :data {:labels (map :date date-sums)
+                                  :datasets [{:data (map date-sum->amount date-sums)
+                                              :fill false}]}
+                           :options {:scales {:xAxes [{:display false}]}
+                                     :legend {:display false}
+                                     :tooltips {:callbacks {:label moneyfy-y-label}}}}]
+
         [:div.container
          [c/collapsing-button collapsed?]
          [:div.row {:class (when @collapsed? "collapse")}
-          [line-chart {:width 400
-                       :height 100
-                       :data {:labels (map :date date-sums)
-                              :datasets [{:label "Money"
-                                          :data (map date-sum->amount date-sums)
-                                          :fill false}]}
-                       :options {:scales {:xAxes [{:display false}]}
-                                 :legend {:display false}}}]]]))))
+          [line-chart chart-options]]]))))
