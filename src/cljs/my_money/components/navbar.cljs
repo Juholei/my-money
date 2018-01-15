b9b26b670c7f0932bd9729007c19fb4365d77e2d
Juho Leinonen <juholei@gmail.com>
2018-01-15 12:56:48 +0200

    Move navbar to its own namespace

---
+++ src/cljs/my_money/components/navbar.cljs
@@ -0,0 +1,44 @@
+(ns my-money.components.navbar
+  (:require [reagent.core :as r]
+            [reagent.session :as session]
+            [my-money.app.controller.authentication :as ac]
+            [my-money.app.controller.navigation :as nc]
+            [my-money.views.login :as login]
+            [my-money.views.registration :as registration]))
+
+(defn nav-link [uri title page collapsed?]
+  [:li.nav-item
+   {:class (when (= page (session/get :page)) "active")}
+   [:a.nav-link
+    {:href uri
+     :on-click #(reset! collapsed? true)} title]])
+
+(defn user-menu [e!]
+  (if-let [user (session/get :identity)]
+    [:ul.navbar-nav.ml-auto
+     [:li.nav-item
+      [:a.btn.btn-outline-danger.btn-sm
+       {:href "#"
+        :on-click #(e! (ac/->Logout))}
+       [:i.fa.fa-user " " user " | log out"]]]]
+    [:ul.navbar-nav.ml-auto
+     [:li.nav-item [login/login-button e!]]
+     [:li.nav-item [registration/registration-button e!]]]))
+
+(defn navbar [e!]
+  (let [collapsed? (r/atom true)]
+    (fn [e!]
+      [:nav#navbar.navbar.navbar-expand-lg.navbar-dark.bg-dark
+       [:a.navbar-brand {:href "#"} "my-money"]
+       [:button.navbar-toggler {:type "button"
+                                :on-click #(swap! collapsed? not)}
+        [:span.navbar-toggler-icon]]
+       [:div.collapse.navbar-collapse
+        (when-not @collapsed? {:class "show"})
+        [:ul.navbar-nav
+         [nav-link "#/" "Home" :home collapsed?]
+         (when (session/get :identity)
+           [:ul.navbar-nav
+            [:button.btn.btn-outline-info.fa.fa-cog.fa-inverse {:on-click #(e! (nc/->OpenModal :config))}]
+            [:button.btn.btn-outline-info {:on-click #(e! (nc/->OpenModal :upload))} "Upload"]])]
+        [user-menu e!]]])))
--- src/cljs/my_money/core.cljs
+++ src/cljs/my_money/core.cljs
@@ -4,14 +4,13 @@
             [secretary.core :as secretary :include-macros true]
             [goog.events :as events]
             [goog.history.EventType :as HistoryEventType]
-            [markdown.core :refer [md->html]]
             [my-money.ajax :refer [load-interceptors!]]
-            [my-money.app.controller.authentication :as ac]
             [my-money.app.controller.alerts :as alerts-controller]
             [my-money.app.controller.navigation :as nc]
             [my-money.app.controller.config :as cc]
             [my-money.app.state :as state]
             [my-money.components.common :as c]
+            [my-money.components.navbar :as navbar]
             [my-money.views.config :as config]
             [my-money.views.registration :as registration]
             [my-money.views.login :as login]
@@ -19,43 +18,6 @@
             [my-money.views.events :refer [events-page]]
             [tuck.core :as tuck])
   (:import goog.History))
-
-(defn nav-link [uri title page collapsed?]
-  [:li.nav-item
-   {:class (when (= page (session/get :page)) "active")}
-   [:a.nav-link
-    {:href uri
-     :on-click #(reset! collapsed? true)} title]])
-
-(defn user-menu [e!]
-  (if-let [user (session/get :identity)]
-    [:ul.navbar-nav.ml-auto
-     [:li.nav-item
-      [:a.btn.btn-outline-danger.btn-sm
-       {:href "#"
-        :on-click #(e! (ac/->Logout))}
-       [:i.fa.fa-user " " user " | log out"]]]]
-    [:ul.navbar-nav.ml-auto
-     [:li.nav-item [login/login-button e!]]
-     [:li.nav-item [registration/registration-button e!]]]))
-
-(defn navbar [e!]
-  (let [collapsed? (r/atom true)]
-    (fn [e!]
-      [:nav#navbar.navbar.navbar-expand-lg.navbar-dark.bg-dark
-       [:a.navbar-brand {:href "#"} "my-money"]
-       [:button.navbar-toggler {:type "button"
-                                :on-click #(swap! collapsed? not)}
-        [:span.navbar-toggler-icon]]
-       [:div.collapse.navbar-collapse
-        (when-not @collapsed? {:class "show"})
-        [:ul.navbar-nav
-         [nav-link "#/" "Home" :home collapsed?]
-         (when (session/get :identity)
-           [:ul.navbar-nav
-            [:button.btn.btn-outline-info.fa.fa-cog.fa-inverse {:on-click #(e! (nc/->OpenModal :config))}]
-            [:button.btn.btn-outline-info {:on-click #(e! (nc/->OpenModal :upload))} "Upload"]])]
-        [user-menu e!]]])))
 
 (defn modal-key->modal [key]
   (condp = key
@@ -69,12 +31,6 @@
   (when-let [opened-modal (modal-key->modal modal-key)]
     [opened-modal e! #(e! (nc/->CloseModal))]))
 
-(defn remove-alert [alert]
-  (fn []
-    (let [correct-alert? (fn [another-alert]
-                           (= (:timestamp alert) (:timestamp another-alert)))]
-      (session/update! :alerts (partial remove correct-alert?)))))
-
 (defn alerts [e! alerts]
   (when (not-empty alerts)
     [:div.container
@@ -86,7 +42,7 @@
   (e! (cc/->RetrieveConfig))
   (fn [e! app]
     [:div
-     [navbar e!]
+     [navbar/navbar e!]
      [c/progress-bar (:in-progress app)]
      [modal e! (:modal app)]
      [alerts e! (:alerts app)]
