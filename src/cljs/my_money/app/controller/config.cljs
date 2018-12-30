9e2ae99d58499c79362e4917cfa7f7ae545c0126
Juho Leinonen <juholei@gmail.com>
2018-12-30 13:52:08 +0200

    Rewrite config updating to use tuck effect

--- src/cljs/my_money/app/controller/config.cljs
+++ src/cljs/my_money/app/controller/config.cljs
@@ -1,20 +1,34 @@
 (ns my-money.app.controller.config
-  (:require [ajax.core :refer [POST]]
-            [my-money.ajax :as ajax]
+  (:require [my-money.ajax :as ajax]
             [tuck.core :as tuck]
             [my-money.app.controller.navigation :as nc]
             [my-money.app.controller.common :as c]
             [my-money.routes :as routes]))
 
 (defrecord SaveConfig [config])
-(defrecord ConfigSaved [])
+(defrecord ConfigSaved [body])
 (defrecord RetrieveConfig [])
 (defrecord SetConfig [config])
 
 (extend-protocol tuck/Event
   SaveConfig
   (process-event [{config :config} app]
-    (tuck/action! (fn [e!]
+    (let [starting-amount-changed? (not= (* (:starting-amount config) 100)
+                                         (:starting-amount app))]
+      (tuck/fx (nc/set-in-progress app true)
+               {:tuck.effect/type ::ajax/post
+                :url              "/save-config"
+                :on-success       ->ConfigSaved
+                :params           (if starting-amount-changed?
+                                    config
+                                    (dissoc config :starting-amount))
+                :on-error         c/->ErrorHandler})
+      #_(POST "/save-config"
+            {:params (if starting-amount-changed?
+                       config
+                       (dissoc config :starting-amount))
+             :handler #(e! (->ConfigSaved))}))
+    #_(tuck/action! (fn [e!]
                     (let [starting-amount-changed? (not= (* (:starting-amount config) 100)
                                                          (:starting-amount app))]
                       (POST "/save-config"
@@ -22,7 +36,7 @@
                                        config
                                        (dissoc config :starting-amount))
                              :handler #(e! (->ConfigSaved))}))))
-    (nc/set-in-progress app true))
+    #_(nc/set-in-progress app true))
 
   ConfigSaved
   (process-event [_ app]
