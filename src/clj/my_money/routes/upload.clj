83a4c113650f899ede59c3ea1b03b3d82b1136ae
Juho Leinonen <juholei@gmail.com>
2020-01-04 19:11:48 +0200

    Refactor bank csv handling into its own smaller functions so it will bea easier to support another bank

--- src/clj/my_money/routes/upload.clj
+++ src/clj/my_money/routes/upload.clj
@@ -1,30 +1,40 @@
 (ns my-money.routes.upload
-  (:require [my-money.db.core :as db]
+  (:require [clojure.string :as str]
+            [my-money.db.core :as db]
             [my-money.op-csv :refer :all]
             [clj-time.jdbc]
             [compojure.core :refer [defroutes POST]]
             [ring.util.http-response :as response]))
+
+(defn ->event [user-id raw-data]
+  {:transaction-id (:Arkistointitunnus raw-data)
+   :user-id user-id
+   :transaction-date (date-string->date (:Kirjauspäivä raw-data))
+   :amount (Integer/parseInt (str/replace (:MääräEUROA raw-data) "," ""))
+   :recipient (:Saaja/Maksaja raw-data)
+   :type (:Laji raw-data)})
 
 (defn- save-events [user-id events]
   (loop [events events
          total-rows 0]
     (if-let [event (first events)]
       (let [inserted-rows (db/create-event!
-                           {:transaction-id (:Arkistointitunnus event)
-                            :user-id user-id
-                            :transaction-date (date-string->date (:Kirjauspäivä event))
-                            :amount (Integer/parseInt (clojure.string/replace (:MääräEUROA event) "," ""))
-                            :recipient (:Saaja/Maksaja event)
-                            :type (:Laji event)})]
+                           (->event user-id event))]
         (recur (rest events) (+ total-rows inserted-rows)))
       (str total-rows))))
+
+(defn csv->clj [csv]
+  (-> csv
+      read-csv
+      csv-vec->map))
+
+(defn ->events [x]
+  (map (partial ->event 1) x))
 
 (defroutes upload-routes
   (POST "/upload" [file :as req]
     (let [user-id (:id (db/get-user-by-username {:username (:identity req)}))
           data (-> (:tempfile file)
                    (slurp :encoding "ISO-8859-1")
-                   (read-csv)
-                   (remove-columns-by-name ["Arvopäivä" "Laji"])
-                   (csv-vec->map))]
-      (-> (response/ok (save-events user-id data))))))
+                   csv->clj)]
+      (response/ok (save-events user-id data)))))
