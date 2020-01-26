928bde4aeb1e4bb0f4bc380f22c2235e05d0f4bf
Juho Leinonen <juholei@gmail.com>
2020-01-26 17:30:17 +0200

    Turn s-pankki data into similar format as op, s-pankki support should now be completely implemented

--- src/clj/my_money/services/spankki_csv.clj
+++ src/clj/my_money/services/spankki_csv.clj
@@ -3,8 +3,22 @@
             [my-money.csv :as csv]
             [my-money.services.bank-csv :as bank]))
 
+(defn ->event [raw-data]
+  (let [amount (Integer/parseInt (str/replace (:Summa raw-data) "," ""))]
+    {:transaction-id (:Arkistointitunnus raw-data)
+     :transaction-date (bank/date-string->date (:Kirjauspäivä raw-data))
+     :amount amount
+     :recipient (if (and (pos? amount) (not= "-" (:Maksaja raw-data)))
+                  (:Maksaja raw-data)
+                  (:Saajannimi raw-data))
+     :type (:Tapahtumalaji raw-data)}))
+
+(defn ->events [data-from-csv]
+  (map ->event data-from-csv))
+
 (defmethod bank/read-bank-statement :spankki [file]
   (-> file
       slurp
       (str/replace "\uFEFF" "")
-      csv/csv->clj))
+      csv/csv->clj
+      ->events))
