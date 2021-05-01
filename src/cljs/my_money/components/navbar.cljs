e009ce210b851b30e131794c7a1d3b81d45a0544
Juho Leinonen <juholei@gmail.com>
2021-05-01 20:08:19 +0300

    Move navbar classes to their own definition

--- src/cljs/my_money/components/navbar.cljs
+++ src/cljs/my_money/components/navbar.cljs
@@ -49,13 +49,25 @@
       [nav-item [registration-button]]])])
 
 
+(def navbar-styles ["mb-2"
+                    "relative"
+                    "flex"
+                    "md:flex-row"
+                    "md:flex-nowrap"
+                    "md:justify-start"
+                    "items-center"
+                    "px-4"
+                    "py-2"
+                    "bg-black"
+                    "bg-opacity-75"])
+
 (defn navbar [e!]
   (r/with-let [collapsed? (r/atom true)]
-    [:nav.mb-2.relative.flex.md:flex-row.md:flex-nowrap.md:justify-start.items-center.px-4.py-2.bg-black.bg-opacity-75
+    [:nav {:class navbar-styles}
      [:a.text-white.inline-block.whitespace-nowrap.text-lg.mr-4 {:href "#"} "my-money"]
      #_[:button.navbar-toggler {:type "button"
-                              :on-click #(swap! collapsed? not)}
-      [:span.navbar-toggler-icon]]
+                                :on-click #(swap! collapsed? not)}
+        [:span.navbar-toggler-icon]]
      ;; TODO: Reimplement .collapse class
      [:div.flex.flex-auto
       (when-not @collapsed? {:class "show"})
