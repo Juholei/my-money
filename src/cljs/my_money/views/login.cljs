d46018d7e1eb8aa062072862cf0712db5be988f3
Juho Leinonen <juholei@gmail.com>
2021-06-06 15:24:51 +0300

    Add preventDefault call to login button back, as it was needed

--- src/cljs/my_money/views/login.cljs
+++ src/cljs/my_money/views/login.cljs
@@ -4,11 +4,16 @@
             [my-money.app.controller.authentication :as ac]
             [reagent.core :as r]))
 
+
+(defn login! [e! data event-handler]
+  (.preventDefault event-handler)
+  (e! (ac/->Login data)))
+
 (defn- buttons [e! data close-fn]
   [:div
    [button {:type :primary
             :form "login"
-            :on-click  (r/partial e! (ac/->Login @data))}
+            :on-click  (r/partial login! e! @data)}
     "Login"]
  [button {:type :danger
           :on-click close-fn}
