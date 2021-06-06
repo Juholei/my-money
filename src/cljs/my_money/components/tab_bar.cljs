4741ee6529425c9020ebefbfbbbaf2bb02563750
Juho Leinonen <juholei@gmail.com>
2021-06-06 15:18:26 +0300

    Add proper roles for tab elements

--- src/cljs/my_money/components/tab_bar.cljs
+++ src/cljs/my_money/components/tab_bar.cljs
@@ -4,8 +4,9 @@
 
 (defn tab [e! active-value value type]
   [:label.btn.btn-primary
-   (when (= value active-value)
-     {:class "active"})
+   {:role "tab"
+    :class [(when (= value active-value)
+              "active")]}
    (string/capitalize value)
    [:input {:type "radio"
             :value value
@@ -15,7 +16,7 @@
 
 (defn tab-bar [e! active-value]
   ;; TODO: Make this generic
-  [:div.btn-group.btn-group-toggle
+  [:div.btn-group.btn-group-toggle {:role "tablist"}
    [tab e! active-value "all" "type"]
    [tab e! active-value "expenses" "type"]
    [tab e! active-value "incomes" "type"]])
