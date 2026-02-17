7df622ea818b4a0bf0c2043571d87c67980988e6
Juho Leinonen <juho.leinonen@solita.fi>
2026-02-17 10:12:34 +0200

    Fix build.clj to explicitly include prod resource and source paths
    The :aliases option in create-basis does not merge extra-paths into
    the basis paths. Explicitly list src and resource dirs including the
    prod env paths, and only clean target/classes instead of all of target
    to preserve frontend build output.

--- build.clj
+++ build.clj
@@ -5,16 +5,19 @@
 (def class-dir "target/classes")
 (def uber-file "my-money.jar")
 (def basis (delay (b/create-basis {:aliases [:prod]})))
+(def src-dirs ["src/clj" "src/cljc" "env/prod/clj"])
+(def resource-dirs ["resources" "env/prod/resources" "target/cljsbuild"])
 
 (defn clean [_]
-  (b/delete {:path "target"}))
+  (b/delete {:path "target/classes"})
+  (b/delete {:path uber-file}))
 
 (defn uber [_]
   (clean nil)
-  (b/copy-dir {:src-dirs   (concat (:paths @basis) ["target/cljsbuild"])
+  (b/copy-dir {:src-dirs   resource-dirs
                :target-dir class-dir})
   (b/compile-clj {:basis     @basis
-                  :src-dirs  ["src/clj" "src/cljc" "env/prod/clj"]
+                  :src-dirs  src-dirs
                   :class-dir class-dir})
   (b/uber {:class-dir class-dir
            :uber-file uber-file
